6df4ae9c0f606ffa1fd9b53c216b22b2f1ca1580
Ronald Das <ronald1das@gmail.com>
2017-01-29 21:29:37 +0530

    Added MP-LAB1

---
+++ MP_Lab/LAB1/q2.asm
@@ -0,0 +1,17 @@
+data segment
+a dw 0002h
+b dw 0004h
+c dw 0000h
+data ends
+code segment
+assume cs:code,ds:data
+start:mov ax,data
+mov ds,ax
+mov ax,a
+mov bx,b
+add ax,bx
+mov c,ax
+mov ah,4ch
+int 21h ; interrupt
+code ends
+end start
